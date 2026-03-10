c6be155c325e9f3627dc897061167093894436d5
LINO-JR <linorepaso@gmail.com>
2026-03-10 22:07:29 +0800

    added gradient background with reverse bg btn

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 console.log("Hello World")
 
 let click = document.getElementById("clickMe")
+let changeBgBtn = document.getElementById("changeBg")
+let reversed = false;
+let ifClicked = false;
+
 
 function clickMe(){
-    click.textContent= "You Clicked Me!"
+    if(!ifClicked){
+        click.textContent= "You Clicked Me!"
+        ifClicked = true;
+    } else {
+        click.textContent= "Click Me!"
+        ifClicked = false;
+    }
+
+    
+
+    // click.textContent= "You Clicked Me!"
+}
+
+function bg(){
+    if(!reversed){
+        document.body.style.background = "linear-gradient(lightblue, pink)";
+        reversed = true;
+    } else {
+        document.body.style.background = "linear-gradient(pink, lightblue)";
+        reversed = false;
+    }
 }
